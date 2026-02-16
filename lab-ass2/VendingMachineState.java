47816260c75e9cf820b1463bccce44c7b43a7f36
Lorenzo Nheo Queñano <enzoquenano993@gmail.com>
2026-02-16 17:40:34 +0800

    Added files

---
+++ lab-ass2/VendingMachineState.java
@@ -0,0 +1,6 @@
+public interface VendingMachineState {
+    void selectItem(VendingMachine vendingMachine);
+    void insertCoin(VendingMachine vendingMachine, int amount);
+    void dispenseItem(VendingMachine vendingMachine);
+    void setOutOfOrder(VendingMachine vendingMachine);
+}
